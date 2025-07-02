b17a395f2b2200a87c7708f4863a79f69e3e6cec
RD <radub33@ulaval.ca>
2025-07-02 19:34:22 -0400

    Minor changes

--- initial_distribution.py
+++ initial_distribution.py
@@ -11,7 +11,6 @@ def initial_sp_distribution(Nx, Ny):
     k_V1 = np.zeros((Nx, Ny))
     k_V2 = np.zeros((Nx, Ny))
 
-
     # Localisation of the V2 vegetation
     V2_loc_x_min = 2
     V2_loc_x_max = 3
@@ -20,20 +19,29 @@ def initial_sp_distribution(Nx, Ny):
 
 
     # Localisation of the V1 vegetation, AROUND the V2 spot 
-    V1_loc_x_min = V2_loc_x_min - 1
-    V1_loc_x_max = V2_loc_x_max + 1
-    V1_loc_y_min = V2_loc_y_min - 1 
-    V1_loc_y_max = V2_loc_y_max + 1
+    # V1_loc_x_min = V2_loc_x_min + 4
+    # V1_loc_x_max = V2_loc_x_max + 4
+    # V1_loc_y_min = V2_loc_y_min - 4
+    # V1_loc_y_max = V2_loc_y_max - 4
+
+
+    V1_loc_x_min = 8
+    V1_loc_x_max = 9
+    V1_loc_y_min = 2
+    V1_loc_y_max = 3
 
 
     # Create the mask inside your initialize() function:
     mask_V2 = np.zeros((Nx, Ny), dtype=bool)
     mask_V2[V2_loc_x_min:V2_loc_x_max, V2_loc_y_min:V2_loc_y_max] = True
+
+    mask_V1 = np.zeros((Nx, Ny), dtype=bool)
+    mask_V1[V1_loc_x_min:V1_loc_x_max, V1_loc_y_min:V1_loc_y_max] = True
     
     # V2[Nx // 3:2*Nx // 3, Ny // 3:2*Ny // 3] = 1*10^5  # Center of the grid
-    V2[V2_loc_x_min:V2_loc_x_max, V2_loc_y_min:V2_loc_y_max] = 1*10^5  # Center of the grid
+    V2[V2_loc_x_min:V2_loc_x_max, V2_loc_y_min:V2_loc_y_max] = 1*10**5  # Center of the grid
     # V1[1:Nx, 1:Ny] = 0  # Center of the grid
-    # V1[V1_loc_x_min:V1_loc_x_max, V1_loc_y_min:V1_loc_y_max] = 1*10^5 
+    V1[V1_loc_x_min:V1_loc_x_max, V1_loc_y_min:V1_loc_y_max] = 1*10**5 
 
     # Localisation of the H2 herbivore
     H2_loc_x_min = 2
@@ -41,25 +49,35 @@ def initial_sp_distribution(Nx, Ny):
     H2_loc_y_min = 2
     H2_loc_y_max = 3
 
+    H1_loc_x_min = 8
+    H1_loc_x_max = 9
+    H1_loc_y_min = 8
+    H1_loc_y_max = 9
+
+    # H1_loc_x_min = V1_loc_x_min - 1
+    # H1_loc_x_max = V1_loc_x_max - 1
+    # H1_loc_y_min = V1_loc_y_min
+    # H1_loc_y_max = V1_loc_y_max
+
     
-    H1[Nx//2, Nx//2] = 0
+    H1[H1_loc_x_min:H1_loc_x_max, H1_loc_y_min:H1_loc_y_max] = 0.1
     H2[H2_loc_x_min:H2_loc_x_max, H2_loc_y_min:H2_loc_y_max] = 0.1
 
     P[Nx//2, Nx//2] = 0
     
     k_V2[V2_loc_x_min:V2_loc_x_max, V2_loc_y_min:V2_loc_y_max] = 167010
-    # k_V1[V1_loc_x_min:V1_loc_x_max, V1_loc_y_min:V1_loc_y_max]= 92870
+    k_V1[V1_loc_x_min:V1_loc_x_max, V1_loc_y_min:V1_loc_y_max]= 92870
 
 
     # Test and put every vegetation to 0
-    k_V1 = np.zeros((Nx, Ny))
+    # k_V1 = np.zeros((Nx, Ny))
     # k_V2 = np.zeros((Nx, Ny))
-    V1 = np.zeros((Nx, Ny))
+    # V1 = np.zeros((Nx, Ny))
     # V2 = np.zeros((Nx, Ny))
 
 
 
-    return V1, V2, H1, H2, P, k_V1, k_V2, mask_V2
+    return V1, V2, H1, H2, P, k_V1, k_V2, mask_V2, mask_V1
 
 
 def plot_species_distribution(species_data, Lx, Ly, species_name='Species'):
