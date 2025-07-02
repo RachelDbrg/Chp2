6833e66f7f82b5eeed360ca1dc9f9d1ad21f0c1e
RD <radub33@ulaval.ca>
2025-07-02 19:33:08 -0400

    Small changes in the diffusion parameters

--- Diffusion_parameters.py
+++ Diffusion_parameters.py
@@ -5,15 +5,15 @@ def initialize_diffusion_parms():
    sigma_H1 = 1
 
    eta_H2 = 10
-   eta_H1 = 1
+   eta_H1 = 10
 
    alpha_H2H2 = 0
    alpha_H1H1 = 0
 
    alpha_H2V1 = 0
-   alpha_H2V2 = 0
+   alpha_H2V2 = 1
    alpha_H1V2 = 0
-   alpha_H1V1 = 0 
+   alpha_H1V1 = 1
 
    alpha_PH2 = 0
    alpha_PH1 = 0
