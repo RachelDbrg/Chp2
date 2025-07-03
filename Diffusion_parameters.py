9decb312ac0fc5f0517f668802d602be00e462d0
RD <radub33@ulaval.ca>
2025-07-03 16:26:28 -0400

    Add the diffusion of the predator

--- Diffusion_parameters.py
+++ Diffusion_parameters.py
@@ -3,19 +3,22 @@ def initialize_diffusion_parms():
 
    sigma_H2 = 1
    sigma_H1 = 1
+   sigma_P = 1
 
-   eta_H2 = 10
-   eta_H1 = 10
+   eta_H2 = 0
+   eta_H1 = 0
+   eta_P = 0
 
    alpha_H2H2 = 0
    alpha_H1H1 = 0
+   alpha_PP = 0
 
    alpha_H2V1 = 0
    alpha_H2V2 = 1
    alpha_H1V2 = 0
    alpha_H1V1 = 1
 
-   alpha_PH2 = 0
-   alpha_PH1 = 0
+   alpha_PH2 = 10
+   alpha_PH1 = 1
 
-   return sigma_H1, sigma_H2, eta_H1, eta_H2, alpha_H2H2, alpha_H1H1, alpha_H2V1, alpha_H2V2, alpha_H1V2, alpha_H1V1, alpha_PH2, alpha_PH1
+   return sigma_H1, sigma_H2, sigma_P, eta_H1, eta_H2, eta_P, alpha_H2H2, alpha_H1H1, alpha_PP, alpha_H2V1, alpha_H2V2, alpha_H1V2, alpha_H1V1, alpha_PH2, alpha_PH1
