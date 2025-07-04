9827be66ba8def1597e8224872d56d9d487450be
RD <radub33@ulaval.ca>
2025-07-04 16:57:55 -0400

    just a bit of tidying and got rid of constant carrying capacity for herbivores

--- parameters_animals.py
+++ parameters_animals.py
@@ -6,46 +6,64 @@ def initialize_animal_parms(k_U_val, k_V_norm, k_U_max):
 
       
     # Prey
-    ## Weights
+    ## ------------------ Weights
     w_H2 = 400
     w_H1 = 100
     
-    ## Carrying capacities: A CHANGER
-    k_H2 = 0.6
-    k_H1 = 0.6 #A CHANGER
+    ## ------------------ Carrying capacities: A CHANGER
+    # k_H2 = 0.6
+    # k_H1 = 0.6 #A CHANGER
 
-    ## Handling times
+    ## ------------------ Handling times
     h_V2H1 = 8.81e-4
     h_V1H2 = 0
     h_V2H2 = 0.000034
     h_V1H1 = 8.81e-4
 
+    ## ------------------ Prospecting area
     a_H1 = 0.05
     a_H2 = 0.05
+
+    ## ------------------ Energy provided by the vegetation
     e_V2 = 20083
     e_V1 = 11.8e3
     
+    ## ------------------ Basal energy needs
     mu_H2 = 70 * w_H2**0.75 * 365 * 1.7 * 4.184
     mu_H1 = 70 * w_H1**0.75 * 365 * 1.7 * 4.184 
 
+    ## ------------------ Proportion of V1 in the H1 diet 
     rho_H1 = 0.6
     
+    ## ------------------ Conversion rates
     epsi_AJ = 0.08
     epsi_H1H2 = w_H1 / w_H2
 
+    ## ------------------ Target growth rates
     target_growth_rate_H2 = 0.39
     target_growth_rate_H1 = 0.26
 
+    ## ------------------ Conversion of energy into new individuals
     chi_H2 = target_growth_rate_H2 / (epsi_AJ) * ((a_H2 * k_U_val * e_V2)/(1 + a_H2 * h_V2H2 * k_U_val) - mu_H2)**-1
-
     denom_supp_NRJ_H1 = 1 + (rho_H1 * a_H1 * h_V1H1 * k_V_norm) + ((1 - rho_H1) * a_H1 * h_V2H1 * k_U_val)
     chi_H1 = target_growth_rate_H1 / (epsi_AJ) * ((((rho_H1 * a_H1 * e_V1 * k_V_norm + (1 - rho_H1) * a_H1 * e_V2 * k_U_max)/(denom_supp_NRJ_H1))) - mu_H1)**-1
 
+    ## ------------------ Intake ratio between prey
+    I_H1 = 0.45 * (w_H1 **0.71)
+    I_H2 = 0.45 * (w_H2 **0.71)
+    gamma_H1H2 = I_H1 / I_H2
 
     # Predator
+    ###  ------------------ Basal energy needs
     mu_P = 0 # set to 0 to avoind redundancy with chi_P
+
+    ## ------------------ Natural mortality ------------------
     phi_P = 30
+
+    ### ------------------ Hunting of the predator ------------------
     h_P = 0
+
+    ### ------------------ Conversion of energy into new individuals ------------------
     chi_P = 0.64/6.93
 
 
@@ -79,7 +97,7 @@ def initialize_animal_parms(k_U_val, k_V_norm, k_U_max):
 
  
     return rho_H1, a_H2, a_H1, h_V2H2, e_V1, e_V2, h_V1H1, h_V2H1, h_V1H2, mu_H2, \
-    mu_H1, epsi_AJ, chi_H2, chi_H1, k_H2, k_H1, a_PH2, a_PH1, h_PH1, h_PH2, mu_P, phi_P, h_P, chi_P, epsi_H1H2
+    mu_H1, epsi_AJ, chi_H2, chi_H1, a_PH2, a_PH1, h_PH1, h_PH2, mu_P, phi_P, h_P, chi_P, epsi_H1H2, gamma_H1H2, I_H1, I_H2
 
 
 
