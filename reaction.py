9f778c0a770fcb69e78da3cc013994e07f3283e9
RD <radub33@ulaval.ca>
2025-07-04 16:56:22 -0400

    forces non-negative growth rates for herbivores

--- reaction.py
+++ reaction.py
@@ -24,9 +24,13 @@ def reaction_eq_prey_mix(V1, V2, P, a_H1, mu_H1, rho_H1, h_V1H1, h_V2H1, e_V1, e
                          h_PH1, a_PH1, H1, H2, h_PH2, a_PH2):
 
 
+    import numpy as np
+
     denom_H = 1 + (rho_H1 * a_H1 * h_V1H1 * V1) + ((1 - rho_H1) * a_H1 * h_V2H1 * V2)
     r_H = (chi_H1 * epsi_AJ * (((rho_H1 * a_H1 * e_V1 * V1 + (1 - rho_H1) * a_H1 * e_V2 * V2)/denom_H) - mu_H1))
-    phi_H = r_H / k_H1
+
+    safe_r_H = np.where(r_H < 1e-6, 1e-16, r_H)
+    phi_H = safe_r_H / k_H1
     denom_predator = 1 + h_PH1 * a_PH1 * H1 + h_PH2 * a_PH2 * H2
     predation_H1 = (a_PH1 * P * H1)/denom_predator
     
@@ -35,17 +39,24 @@ def reaction_eq_prey_mix(V1, V2, P, a_H1, mu_H1, rho_H1, h_V1H1, h_V2H1, e_V1, e
 
     return reaction_H
 
+
 def reaction_eq_prey_mono(a_H2, h_V2H2, V2, chi_H2, epsi_AJ, e_V2, mu_H2, k_H, a_PH2, P, H2, h_PH1, a_PH1, H1, h_PH2):
+
+    import numpy as np
 
     denom_H2 = (1 + a_H2 * h_V2H2 * V2)
     r_H = (chi_H2 * epsi_AJ * ((a_H2 * e_V2 * V2)/(denom_H2) - mu_H2))
-    phi_H = r_H / k_H
+
+    safe_r_H = np.where(r_H < 1e-6, 1e-16, r_H)
+
+    phi_H = safe_r_H / k_H
     denom_predator = 1 + h_PH1 * a_PH1 * H1 + h_PH2 * a_PH2 * H2
     predation_H2 = (a_PH2 * P * H2)/denom_predator
 
     reaction_H = r_H * H2 - phi_H * H2**2 + predation_H2
 
     return reaction_H, r_H
+
 
 def reaction_eq_predator(P, H1, H2, a_PH1, a_PH2, h_PH1, h_PH2, phi_P, h_P, chi_P, epsi_H1H2, mu_P) :
 
