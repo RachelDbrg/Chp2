dcde4a592d2731a90dc348fde6852b4d94c465d6
RD <radub33@ulaval.ca>
2025-07-08 13:19:36 -0400

    made it work with season dependant diffusion

--- Diffusion_parameters.py
+++ Diffusion_parameters.py
@@ -4,7 +4,7 @@ def initialize_diffusion_parms():
    # Sensitivity of species for high G scores
    # Serves as a weighting factor. Default to 0.5 so omega = 1
    sigma_H2 = 0.5
-   sigma_H1 = 0.5
+   # sigma_H1 = 0.5
    sigma_P = 0.5
 
    # Average movement of species, area prospected per year (km2/year)
@@ -12,7 +12,7 @@ def initialize_diffusion_parms():
    # based on the quality of the patch
 
    eta_H2 = 0.05
-   eta_H1 = 0.05
+   # eta_H1 = 0.05
    eta_P = 65
 
    # Intra-specific diffusion term: should be <0
@@ -30,4 +30,5 @@ def initialize_diffusion_parms():
    alpha_PH2 = 1
    alpha_PH1 = 1
 
-   return sigma_H1, sigma_H2, sigma_P, eta_H1, eta_H2, eta_P, alpha_H2H2, alpha_H1H1, alpha_PP, alpha_H2V1, alpha_H2V2, alpha_H1V2, alpha_H1V1, alpha_PH2, alpha_PH1
+   # return sigma_H1, sigma_H2, sigma_P, eta_H1, eta_H2, eta_P, alpha_H2H2, alpha_H1H1, alpha_PP, alpha_H2V1, alpha_H2V2, alpha_H1V2, alpha_H1V1, alpha_PH2, alpha_PH1
+   return sigma_H2, sigma_P, eta_H2, eta_P, alpha_H2H2, alpha_H1H1, alpha_PP, alpha_H2V1, alpha_H2V2, alpha_H1V2, alpha_H1V1, alpha_PH2, alpha_PH1
