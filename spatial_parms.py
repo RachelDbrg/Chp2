c6e3d8ee9acfd182f8454a26636dafe52cac6e24
RD <radub33@ulaval.ca>
2025-07-14 17:34:08 -0400

    minor changes

--- spatial_parms.py
+++ spatial_parms.py
@@ -4,12 +4,11 @@ def initialize_spatial_parms():
    
     
  # Grid and time setup
-    Nx, Ny = 10, 10    # Number of grid cells in the x- and y-direction
-    Lx, Ly = 10000, 10000   # Physical length of the domain in the x- and y-direction (e.g., meters, kilometers)
+    Nx, Ny = 30, 30    # Number of grid cells in the x- and y-direction
+    Lx, Ly = 100, 100   # Physical length of the domain in the x- and y-direction (e.g., meters, kilometers)
     dx, dy = Lx/Nx, Ly/Ny # Spatial resolution (grid cell size)
     dt = 0.01      # time step size (in years)
     T = 100.0      # total simulation time (in years)
     Nt = int(T / dt)  # number of time steps
-
 
     return Nx, Ny, dx, dy, dt, Nt, Lx, Ly
